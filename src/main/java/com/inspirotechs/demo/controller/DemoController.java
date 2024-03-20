a0984887cfe0610fec869a7adfc86bf75820af45
Nur Kholid <cocokcocok33@gmail.com>
2024-03-20 13:21:19 +0700

    get,delete,add operations

--- src/main/java/com/inspirotechs/demo/controller/DemoController.java
+++ src/main/java/com/inspirotechs/demo/controller/DemoController.java
@@ -8,7 +8,11 @@ import com.inspirotechs.demo.repository.EmployeeRepository;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class DemoController {
@@ -19,6 +23,24 @@ public class DemoController {
     @GetMapping("/api/employees")
     List<Employee> all() {
         return repository.findAll();
+    }
+
+    @PostMapping("/api/employees")
+    public Employee saveEmployee(@RequestBody Employee entity) {
+        return repository.save(entity);
+    }
+
+    @DeleteMapping("/api/employee")
+    public String deleteEmployee(@RequestParam Long id) {
+        try {
+            if (repository.existsById(id))
+                repository.deleteById(id);
+            else
+                throw new IllegalArgumentException("Employee not found");
+        } catch (IllegalArgumentException e) {
+            return e.getMessage();
+        }
+        return "Delete Success";
     }
 
     @GetMapping("/")
--- src/main/java/com/inspirotechs/demo/entity/Employee.java
+++ src/main/java/com/inspirotechs/demo/entity/Employee.java
@@ -4,11 +4,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 /**
  * employee
  */
 @Entity
+@Table(name = "employee")
 public class Employee {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
